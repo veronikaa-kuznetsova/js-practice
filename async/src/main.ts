436aa07a18eabb749eec092daab68ed6c6d4d32d
Вероника <69638667+veronikaa-kuznetsova@users.noreply.github.com>
2025-02-11 08:35:40 +0700

    Update main.ts

--- async/src/main.ts
+++ async/src/main.ts
@@ -18,37 +18,39 @@ function createCheckUrlResult(url: string, available: boolean): CheckUrlResult {
     };
 }
 
-async function checkUrls(urls: string[], processInfo: ProcessInfo): Promise<[CheckUrlResult[], ProcessInfo]> {
-    const results: CheckUrlResult[] = [];
+async function checkUrl(url: string): Promise<CheckUrlResult> {
+    try {
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), 10000);
 
+        const response = await fetch(url, { signal: controller.signal });
+
+        clearTimeout(timeoutId);
+        if (response.ok) {
+            return createCheckUrlResult(url, true);
+        } else {
+            return createCheckUrlResult(url, false);
+        }
+    } catch (error) {
+        return createCheckUrlResult(url, false);
+    }
+}
+
+async function checkUrls(urls: string[], processInfo: ProcessInfo): Promise<[CheckUrlResult[], ProcessInfo]> {
     processInfo.totalRequests = urls.length;
     processInfo.completedRequests = 0;
     processInfo.status = 'inprogress';
 
-    for (const url of urls) {
-        try {
-            const controller = new AbortController();
-            const timeoutId = setTimeout(() => controller.abort(), 10000);
-
-            const response = await fetch(url, { signal: controller.signal });
-
-            clearTimeout(timeoutId);
-            if (response.ok) {
-                results.push(createCheckUrlResult(url, true));
-            } else {
-                results.push(createCheckUrlResult(url, false));
-            }
-
-            processInfo.completedRequests++;
-        } catch (error) {
-            results.push(createCheckUrlResult(url, false));
-            processInfo.completedRequests++;
-        }
+    try {
+        const results = await Promise.all(urls.map(url => checkUrl(url)));
+        processInfo.completedRequests = results.length;
+        processInfo.status = 'completed';
+        return [results, processInfo];
+    } catch (error) {
+        console.error('error', error);
+        processInfo.status = 'completed';
+        return [[], processInfo];
     }
-
-    processInfo.status = 'completed';
-
-    return [results, processInfo];
 }
 
 (async () => {
