e095e7d6533171d0f750a1353d0c3572e3ce8f05
Veronika Kuznetsova <djveronika@mail.ru>
2025-02-23 23:49:20 +0700

    Parse nested structure

--- nested-structure/parse.ts
+++ nested-structure/parse.ts
@@ -1,3 +1,16 @@
 export const parseStruct = ( data: unknown ) => {
-
+  if (typeof data === 'object' && data !== null) {
+    return Object.keys(data).reduce((accumulator, key) => {
+      if (key === 'text') {
+        accumulator.push(data[key])
+      } else {
+        accumulator.push(...parseStruct(data[key]))
+      }
+      return accumulator
+    }, [])
+  } else if (Array.isArray(data)) {
+    return data.flatMap(parseStruct)
+  } else {
+    return []
+  }
 }
