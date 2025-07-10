bc7674b284c83895409553625a81c95d14440a92
Veronika Kuznetsova <djveronika@mail.ru>
2025-07-10 23:25:11 +0700

    feat: add loading processing, errors

--- typical-solutions/src/pages/forms/index.js
+++ typical-solutions/src/pages/forms/index.js
@@ -1,29 +1,67 @@
 import load from '../load.js';
 
 export default function Forms() {
-  function serializeForm(formNode) {
-    const data = new FormData(formNode);
-    console.log(Array.from(data.entries()));
-  }
-
   async function handleFormSubmit(event) {
-    event.preventDefault();
-    const data = serializeForm(event.target);
+    event.preventDefault()
+    const data = serializeForm(event.target)
 
-    const response = await sendData(data);
+    toggleLoader(event.target)
+    const { status, error } = await sendData(data)
+    toggleLoader(event.target)
+
+    if (status === 200) {
+      onSuccess(event.target)
+    } else {
+      onError(error)
+    }
   }
 
   async function sendData(data) {
-    return await fetch('', {
+    return await fetch('/', {
       method: 'POST',
-      body: data,
+      body: data
     })
+  }
+
+  function serializeForm(formNode) {
+    return new FormData(formNode);
+  }
+
+  function onSuccess(formNode) {
+    alert('Поздравляем, вы подписали NDA');
+    formNode.classList.toggle('hidden');
+  }
+
+  function checkValidate(event) {
+    const formNode = event.target.form;
+    const isValidate = formNode.checkValidity();
+
+    formNode.querySelector('.btn').disabled = !isValidate;
+  }
+
+  function toggleLoader(formNode) {
+    const loader = formNode.querySelector('.loader');
+    const btnText = formNode.querySelector('.btn-text');
+    const btn = formNode.querySelector('.btn');
+
+    if (btn.disabled) {
+      loader.classList.add('hidden');
+      btnText.classList.remove('hidden');
+    } else {
+      loader.classList.remove('hidden');
+      btnText.classList.add('hidden');
+    }
   }
 
   load('forms', () => {
     const form = document.querySelector('.form');
+
     if (form) {
       form.addEventListener('submit', handleFormSubmit);
+      const inputs = form.querySelectorAll('input');
+      inputs.forEach(input => {
+        input.addEventListener('input', checkValidate);
+      });
     } else {
       console.error('Форма не найдена');
     }
